9679df0c49eb38bb67d2db8a23fbac9383220896
João Vitor Castilho <joao.castilho@mstech.com.br>
2023-02-01 12:40:05 -0300

    try-catch improved

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -13,22 +13,23 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter the account data:");
+        System.out.print("Number: ");
+        int number = input.nextInt();
+        System.out.print("Holder: ");
+        input.nextLine();
+        String holder = input.nextLine();
+        System.out.print("Initial balance: ");
+        double balance = input.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = input.nextDouble();
+
+        Account account = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.print("\nEnter amount for withdraw: ");
+        double amount = input.nextDouble();
+
         try{
-            System.out.println("Enter the account data:");
-            System.out.print("Number: ");
-            int number = input.nextInt();
-            System.out.print("Holder: ");
-            input.nextLine();
-            String holder = input.nextLine();
-            System.out.print("Initial balance: ");
-            double balance = input.nextDouble();
-            System.out.print("Withdraw limit: ");
-            double withdrawLimit = input.nextDouble();
-
-            Account account = new Account(number, holder, balance, withdrawLimit);
-
-            System.out.print("\nEnter amount for withdraw: ");
-            double amount = input.nextDouble();
             account.withdraw(amount);
             System.out.print("New balance: " + account.getBalance());
         }
